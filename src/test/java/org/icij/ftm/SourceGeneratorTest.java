6dc3ebeb3abe2cf5a76f366c706936d571aad0ac
Bruno Thomas <bthomas@icij.org>
2024-10-16 14:21:00 +0000

    test: add test for int

--- src/test/java/org/icij/ftm/SourceGeneratorTest.java
+++ src/test/java/org/icij/ftm/SourceGeneratorTest.java
@@ -22,6 +22,12 @@ public class SourceGeneratorTest {
         assertThat(new SourceGenerator().generate(path)).contains("public record Thing(String name);");
     }
 
+    @Test
+    public void test_generate_with_int_property() throws IOException {
+        Path path = getPath("Int.yaml");
+        assertThat(new SourceGenerator().generate(path)).contains("public record Int(int number);");
+    }
+
     private static Path getPath(String name) {
         return Paths.get(ClassLoader.getSystemResource(name).getPath());
     }
